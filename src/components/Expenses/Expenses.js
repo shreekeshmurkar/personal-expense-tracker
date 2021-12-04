a5761bccd271fc54394b18d10bde0ee800d1850e
shreekeshmurkar <shreekeshmurkar@gmail.com>
2021-12-04 19:03:45 +0530

    added changes related to conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
@@ -12,18 +13,7 @@ function Expenses(props) {
     (item) => item.date.getFullYear().toString() === filteredYear
   );
 
-  let expenseContent = <p>No expense found</p>;
-
-  if(filteredExpenses.length > 0 ){
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    )); 
-  }
+  
 
   return (
     <Card className="expenses">
@@ -31,7 +21,7 @@ function Expenses(props) {
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
-      {expenseContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
