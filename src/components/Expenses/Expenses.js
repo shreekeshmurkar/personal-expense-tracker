98c1214e649d831b6142273933d41d392eb52d33
shreekeshmurkar <shreekeshmurkar@gmail.com>
2021-12-04 22:35:45 +0530

    Added changes related to bar chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -13,14 +14,13 @@ function Expenses(props) {
     (item) => item.date.getFullYear().toString() === filteredYear
   );
 
-  
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
